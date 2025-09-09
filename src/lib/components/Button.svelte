56c077844984f8356a42e522cc1e40e8be0fce44
David Hurtado Henao <david@b560.b560>
2025-09-09 16:14:51 -0500

    feat: Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		action: () => void;
+		bg?: string;
+		children?: any;
+	}
+
+	let { action, children, bg }: Props = $props();
+</script>
+
+<button
+	class="cursor-pointer rounded-md bg-teal-700 px-4 py-2 font-semibold text-white"
+	onclick={action}>{@render children()}</button>
