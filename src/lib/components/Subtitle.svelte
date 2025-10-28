cfb1aa4cbb82eff46a1366ca93a626e52a6efe6d
David Hurtado Henao <david@b560.b560>
2025-10-28 08:15:42 -0500

    feat: title components

---
+++ src/lib/components/Subtitle.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<h1 class="w-full text-lg font-semibold">{@render children()}</h1>
---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<h1 class="w-full text-center text-lg font-bold">{@render children()}</h1>
