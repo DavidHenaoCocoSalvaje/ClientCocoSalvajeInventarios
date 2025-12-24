314b23d4ddb01b67c716eeab9584a5121fc59b43
David Hurtado Henao <david@b560.b560>
2025-12-24 09:27:38 -0500

    feat: settings button

---
+++ src/routes/aplicaciones/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Title from '$lib/components/Title.svelte';
+</script>
+
+<div class="flex h-full w-full flex-col px-10 pb-10 pt-24">
+	<Title>Aplicaciones</Title>
+	<p class="mt-4 text-gray-600">Gestión de aplicaciones y autorizaciones OAuth.</p>
+</div>
---
+++ src/routes/permisos/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Title from '$lib/components/Title.svelte';
+</script>
+
+<div class="flex h-full w-full flex-col px-10 pb-10 pt-24">
+	<Title>Permisos</Title>
+	<p class="mt-4 text-gray-600">Panel de administración de permisos.</p>
+</div>
