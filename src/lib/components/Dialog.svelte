3d1d26fe11efa2842f090e6da5934289821d6b3e
David Hurtado Henao <david@b560.b560>
2025-12-24 11:03:11 -0500

    refactor: Section, Title components

--- src/lib/components/Dialog.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    let { text } = $props()
-</script>
-
-<dialog>
-    <p>{{ text }}</p>
-</dialog>
---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<section class="sticky top-0 z-20 flex w-full flex-col items-center gap-5 bg-white pt-5">
+	{@render children()}
+</section>
