2da38595654e622e20dd385896e592d9943e7764
David Hurtado Henao <david@b560.b560>
2026-01-12 10:47:25 -0500

    feat: inicio panel permisos

---
+++ src/lib/components/MainSection.svelte
@@ -0,0 +1,13 @@
+<!-- Se utiliza para renderizar el contenido debajo del header -->
+
+<script lang="ts">
+	interface Props {
+		children?: any;
+		className?: string;
+	}
+	let { children, className }: Props = $props();
+</script>
+
+<section class="flex w-full flex-col overflow-auto  bg-white mt-14 {className}">
+	{@render children()}
+</section>
